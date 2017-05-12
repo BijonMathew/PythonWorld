15664e51f4abbbe8e5e6fa4dd469a8a2738f9265
BijonMathew <bijonmathew@gmail.com>
2017-05-12 09:42:43 +0530

    Added a print statement

--- gitpython.py
+++ gitpython.py
@@ -1,3 +1,5 @@
 import webbrowser
 
+print("Hey, watch this Python promo")
+
 webbrowser.open("https://www.bing.com/videos/search?q=what+is+python+programming&&view=detail&mid=DBE600CF56BEAA173A5ADBE600CF56BEAA173A5A&FORM=VRDGAR")
